32d18e69db590dd8c675d7875a325280664d25e4
cedricpriestley <cedricpriestley@gmail.com>
2019-12-23 16:15:30 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+# http(s) link to package home page.
+TERMUX_PKG_HOMEPAGE=https://github.com/cedricpriestley/termux-nodev6
+
+# One-line, short package description.
+TERMUX_PKG_DESCRIPTION="NodeJS V6 Build for Termux"
+
+# License.
+TERMUX_PKG_LICENSE="GPL-2.0"
+
+# Version.
+TERMUX_PKG_VERSION=1.15
+
+# URL to archive with source code.
+TERMUX_PKG_SRCURL=https://nodejs.org/dist/v6.12.2/node-v6.12.2-linux-arm64.tar.xz
+
+# SHA-256 checksum of the source code archive.
+TERMUX_PKG_SHA256=a9b37f4ad08ffbb86dd2c20e0d86504cfb9131f804167fbd61bd4c6a706e1a09
